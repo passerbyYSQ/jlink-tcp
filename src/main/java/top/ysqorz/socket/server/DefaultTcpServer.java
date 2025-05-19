e9b4af8680b715025feaad0f96f0f4e6f9b8b450
姚世权 <yaoshiquan@zwcad.com>
2025-05-19 13:52:02 +0800

    动态代理ClientHandler的close方法，以便在外部关闭时能够将自身从TcpServer的Map中移除

--- src/main/java/top/ysqorz/socket/server/DefaultTcpServer.java
+++ src/main/java/top/ysqorz/socket/server/DefaultTcpServer.java
@@ -1,9 +1,9 @@
 package top.ysqorz.socket.server;
 
-import top.ysqorz.socket.io.exception.ClientException;
 import top.ysqorz.socket.io.ExceptionHandler;
 import top.ysqorz.socket.io.NamedThreadFactory;
 import top.ysqorz.socket.io.ReceivedCallback;
+import top.ysqorz.socket.io.exception.ClientException;
 import top.ysqorz.socket.io.packet.AckReceivedPacket;
 import top.ysqorz.socket.io.packet.FileReceivedPacket;
 import top.ysqorz.socket.io.packet.StringReceivedPacket;
@@ -12,6 +12,9 @@ import top.ysqorz.socket.log.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
@@ -53,12 +56,11 @@ public class DefaultTcpServer implements TcpServer, ReceivedCallback, ExceptionH
 
     @Override
     public void removeClient(String clientId) throws IOException {
-        ClientHandler handler = clientHandlerMap.remove(clientId);
+        ClientHandler handler = clientHandlerMap.get(clientId);
         if (Objects.isNull(handler)) {
             return;
         }
-        handler.close();
-        log.info(String.format("Client count: %d. Remove client: %s", clientHandlerMap.size(), handler.getClientInfo()));
+        handler.close(); // close已经被代理，会将自身从Map中移除
     }
 
     @Override
@@ -87,8 +89,8 @@ public class DefaultTcpServer implements TcpServer, ReceivedCallback, ExceptionH
 
     @Override
     public void close() throws IOException {
-        for (String clientId : clientHandlerMap.keySet()) {
-            removeClient(clientId);
+        for (ClientHandler clientHandler : clientHandlerMap.values()) {
+            clientHandler.close();
         }
         if (Objects.nonNull(serverSocket)) {
             serverSocket.close();
@@ -134,6 +136,28 @@ public class DefaultTcpServer implements TcpServer, ReceivedCallback, ExceptionH
         handler.sendText("Successfully accept " + handler.getClientInfo().toString()); // 启动后回送消息
     }
 
+    private class ClientInvocationHandler implements InvocationHandler {
+        final DefaultClientHandler clientHandler;
+
+        ClientInvocationHandler(DefaultClientHandler clientHandler) {
+            this.clientHandler = clientHandler;
+        }
+
+        /**
+         * 代理DefaultClientHandler的close方法
+         */
+        @SuppressWarnings("resource")
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            if ("close".equals(method.getName())) {
+                ClientInfo clientInfo = clientHandler.getClientInfo();
+                clientHandlerMap.remove(clientInfo.getClientId());
+                log.info(String.format("Client count: %d. Remove client: %s", clientHandlerMap.size(), clientInfo));
+            }
+            return method.invoke(clientHandler, args);
+        }
+    }
+
     private class Acceptor extends Thread {
         Acceptor(String name) throws SocketException {
             super(name);
@@ -146,7 +170,7 @@ public class DefaultTcpServer implements TcpServer, ReceivedCallback, ExceptionH
             while (true) {
                 try {
                     Socket socket = serverSocket.accept();
-                    DefaultClientHandler clientHandler = new DefaultClientHandler(socket, ackTimeoutScanner); // 所有客户端处理器共用一个Ack超时扫描线程
+                    ClientHandler clientHandler = newClientHandlerProxy(socket); // 所有客户端处理器共用一个Ack超时扫描线程
                     ClientInfo clientInfo = clientHandler.getClientInfo();
                     clientHandlerMap.put(clientInfo.getClientId(), clientHandler);
                     log.info(String.format("Client count: %d. Accept client: %s", clientHandlerMap.size(), clientInfo));
@@ -163,6 +187,12 @@ public class DefaultTcpServer implements TcpServer, ReceivedCallback, ExceptionH
                     break;
                 }
             }
+        }
+
+        ClientHandler newClientHandlerProxy(Socket socket) throws IOException {
+            DefaultClientHandler clientHandler = new DefaultClientHandler(socket, ackTimeoutScanner);
+            return (ClientHandler) Proxy.newProxyInstance(clientHandler.getClass().getClassLoader(),
+                    new Class[]{ClientHandler.class}, new ClientInvocationHandler(clientHandler));
         }
     }
 }
